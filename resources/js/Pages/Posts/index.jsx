a160e95279519b152fcd8b1d584fae3a02341cf0
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2024-04-14 16:38:45 +0600

    react connected

---
+++ resources/js/Pages/Posts/index.jsx
@@ -0,0 +1,7 @@
+export default function PostsIndex() {
+    return (
+        <div>
+            <h1>Posts Index</h1>
+        </div>
+    );
+}
---
+++ resources/js/app.jsx
@@ -0,0 +1,7 @@
+import './bootstrap';
+import ReactDOM from 'react-dom/client';
+import PostsIndex from './Pages/Posts';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <PostsIndex />
+);
